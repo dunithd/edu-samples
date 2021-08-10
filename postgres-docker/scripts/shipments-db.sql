bf5accba3c80ed70ad53d6a0216ef34741d4a2f5
dunithd <dunithd@gmail.com>
2021-08-10 14:43:58 +0530

    Adding the Postgres+Debezium+Docker sample

---
+++ postgres-docker/scripts/shipments-db.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE IF NOT EXISTS shipments
+(
+    shipment_id bigint NOT NULL,
+    order_id bigint NOT NULL,
+    date_created character varying(255) COLLATE pg_catalog."default",
+    status character varying(25) COLLATE pg_catalog."default",
+    CONSTRAINT shipments_pkey PRIMARY KEY (shipment_id)
+);
+
+
+INSERT INTO shipments values (30500,10500,'2021-01-21','COMPLETED');
+INSERT INTO shipments values (31500,11500,'2021-04-21','COMPLETED');
+INSERT INTO shipments values (32500,12500,'2021-05-31','PROCESSING');
